1a90b28ec97c528fa546155fa7e099a58857906b
grasskill <grasskill@ubuntu>
2015-02-12 13:03:24 -0800

    GUI implementatie

--- NetworkMonitor/src/networkmonitor.py
+++ NetworkMonitor/src/networkmonitor.py
@@ -3,3 +3,14 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+import curses #GUI interface
+
+myscreen = curses.initscr() #Initialize GUI
+
+myscreen.border(0) #GUI border
+myscreen.addstr(12, 25, "Welkom bij de Netwerk Monitor van DD&M!") #GUI 
+myscreen.refresh() #Refresh GUI
+myscreen.getch()
+
+curses.endwin() 
+
